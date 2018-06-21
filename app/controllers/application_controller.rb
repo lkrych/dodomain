f00f3aa0a5f8a37a1a58646dfa7f49d5df509e8c
Leland Krych <Leland.Krych@gmail.com>
2018-06-21 15:37:45 -0700

    add authenticate request validation to application_Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,35 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+  def authenticate_request!
+    unless user_id_in_token?
+      respond_to do |format|
+        format.html { render file: "#{Rails.root}/public/404.html" , status: 404 }
+        format.json { render json: { error: 'Not Authenticated' }, status: :unauthorized }
+      end
+      return
+    end
+    @current_user = User.find(auth_token[:user_id])
+
+  end
+
+  private
+  def http_token
+    if request.headers['Authorization'].present?
+      @http_token = request.headers['Authorization'].split(' ').last
+    elsif request.params[:token].present?
+      @http_token = request.params[:token]
+    else
+      return false;
+    end
+  end
+
+  def auth_token
+    @auth_token = JsonWebToken.decode(http_token)
+  end
+
+  def user_id_in_token?
+    http_token && auth_token && auth_token[:user_id].to_i
+  end
 end
