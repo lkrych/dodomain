a8bf86111e32d6b8f10cbca8ba8940c6353aa4d8
Leland Krych <Leland.Krych@gmail.com>
2018-06-21 14:36:27 -0700

    add routes for controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'users/create'
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:new, :create]
+      resources :domains, only: [:index, :create]
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
