2388e85f91e5dc7e8da594679d7dd69110677f0f
Leland Krych <Leland.Krych@gmail.com>
2018-06-21 15:38:42 -0700

    create user controller methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,21 @@
 class Api::v1::UsersController < Api::v1::BaseController
+  
   def new
+    @user = User.new 
   end
 
   def create
+    @user = User.new(params[:user]) #use params to create user
+    begin 
+      @user.save #return user if there are no errors
+      render json: {
+        user: @user, status: :ok
+      }
+    rescue => e #return an error if there is an error
+      render json: {
+        error: e, status: :bad_request
+      }
+    end
   end
+
 end
