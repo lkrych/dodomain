38685d53299875f7e8e2438c981e48b3ddd51816
Leland Krych <Leland.Krych@gmail.com>
2018-06-21 15:39:48 -0700

    Add auth endpoints to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:new, :create]
       resources :domains, only: [:index, :create]
+      
+      post 'auth' => 'authentication#get_token'
+      post 'signup' => 'authentication#sign_up'
+      delete 'auth' => 'authentication#sign_out'
+      
     end
   end
 
