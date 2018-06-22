d5cabfc1f3905055255b0a42df5304c027265be8
Leland Krych <Leland.Krych@gmail.com>
2018-06-22 07:54:54 -0700

    catch expired JWT

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,8 +4,9 @@ class JsonWebToken
   end
 
   def self.decode(token)
-    return HashWithIndifferentAccess.new(JWT.decode(token, Rails.application.secrets.secret_key_base)[0])
-  rescue
-    nil
-  end
+    begin
+      return HashWithIndifferentAccess.new(JWT.decode(token, Rails.application.secrets.secret_key_base)[0])
+    rescue JWT::ExpiredSignature
+      return JWT::ExpiredSignature
+    end
 end
