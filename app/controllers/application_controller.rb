098c24b71572e3eb0ea7264f26a360cc2e6c5bf5
Leland Krych <Leland.Krych@gmail.com>
2018-06-22 11:09:27 -0700

    make sure error in JWT returns proper error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     unless user_id_in_token?
       respond_to do |format|
         format.html { render file: "#{Rails.root}/public/404.html" , status: 404 }
-        format.json { render json: { error: 'Not Authenticated' }, status: :unauthorized }
+        format.json { render json: { errors: 'Not Authenticated' }, status: :unauthorized }
       end
       return
     end
@@ -30,6 +30,10 @@ class ApplicationController < ActionController::Base
   end
 
   def user_id_in_token?
-    http_token && auth_token && auth_token[:user_id].to_i
+    begin
+      http_token && auth_token && auth_token[:user_id].to_i
+    rescue 
+      return false
+    end
   end
 end
