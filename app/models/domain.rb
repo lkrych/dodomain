a7baa2e85219a6b0cca5e13c79d292de90e60c20
Leland Krych <Leland.Krych@gmail.com>
2018-06-22 16:51:34 -0700

    create domain instance methods

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,9 +1,28 @@
 class Domain < ApplicationRecord
-  #add association
+  require 'socket'
+  
   belongs_to :user
-  #add validations
 
   def self.search(query)
     where("name LIKE ? ", "%#{query}%")
+  end
+
+  def parse_domain
+    begin
+      uri = URI.parse(name) #use ruby stdlib to create URI
+      domain = PublicSuffix.parse(uri.host) #uses PublicSuffixList https://publicsuffix.org/
+      self.name = domain.domain
+    rescue
+      raise "Not a valid domain!"
+    end
+  end
+
+  def check_if_valid_domain #make request to DNS Lookup API
+    begin
+      IPSocket::getaddress(name)
+      return true
+    rescue
+      return false
+    end
   end
 end
