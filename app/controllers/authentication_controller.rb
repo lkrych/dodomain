564a02d7a555397443374e3fa9b59adcdf430fbd
Leland Krych <Leland.Krych@gmail.com>
2018-06-22 07:53:02 -0700

    Add strong params to auth controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,13 +2,13 @@ class AuthenticationController < ApplicationController
   before_action :authenticate_request!, only: :sign_out
 
   def get_token
-    email = params[:email]
+    email = user_params[:email]
     user = User.where(email: email).first
     if(user.nil?)
-      return send_error('Email was not found')
+      return send_error('There is no account associated with this email')
     end
     
-    if user.match_password(params[:password])
+    if user.match_password(user_params[:password])
       return send_token(user)
     else
       send_error('Incorrect Password')
@@ -17,21 +17,31 @@ class AuthenticationController < ApplicationController
 
 
   def sign_up
-    email = params[:email]
+    email = user_params[:email]
     user = User.where(email: email).first
     if user
-      return send_error('This email has already been used')
+      return send_error('An account has already been created with this email')
     end
-    user.password = params[:password]
-    user.save
-    send_token(user)
-  end
 
+    user = User.new(user_params)
+    begin 
+      user.save
+      send_token(user)
+    rescue => e #return an error if there is an error
+      send_error(e)
+    end
+  end
+    
   def sign_out
     render json: {auth_token: ''}
   end
 
   private
+
+  #use strong params to prevent anyone from sending in malicious attributes
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 
   def send_token(user)
     render json: payload(user)
@@ -43,9 +53,11 @@ class AuthenticationController < ApplicationController
 
   def payload(user)
     return nil unless user and user.id
+    exp = Time.now.to_i + 24 * 3600 #set expiration to one day
     {
       auth_token: JsonWebToken.encode({
         user_id: user.id,
+        exp: exp 
       })
     }
   end
