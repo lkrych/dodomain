37973464087c2522d6f611fd87155da0f6020f29
Leland Krych <Leland.Krych@gmail.com>
2018-06-21 22:47:16 -0700

    denamespace routes for simplicity

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:new, :create]
-      resources :domains, only: [:index, :create]
+
+  resources :users, only: [:new, :create]
+  resources :domains, only: [:index, :create]
+  
+  post 'auth' => 'authentication#get_token'
+  post 'signup' => 'authentication#sign_up'
+  delete 'auth' => 'authentication#sign_out'
       
-      post 'auth' => 'authentication#get_token'
-      post 'signup' => 'authentication#sign_up'
-      delete 'auth' => 'authentication#sign_out'
-      
-    end
-  end
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
