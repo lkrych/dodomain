2836c437ebfd69d4c8c6057d4ae71947f40b3de2
Leland Krych <Leland.Krych@gmail.com>
2018-06-22 07:52:46 -0700

    adjust tests in authentication_controller spec

--- spec/controllers/authentication_controllerer_spec.rb
+++ spec/controllers/authentication_controllerer_spec.rb
@@ -1,42 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe AuthenticationController, type: :controller do
+  #helper method for decoding JWT
   def decodeResponseJWT(response)
     token = json(response)['auth_token']
     return JsonWebToken.decode(token)
   end
+
+  before :each do  
+    @u = FactoryBot.create(:user)
+  end
   
   context 'Logging in' do
+
     context 'Valid credentials' do
 
       it 'should return a valid JWT ' do
-        u = FactoryBot.create(:user)
-        post :get_token, params: {:email => "firstuser@gmail.com",
-                    :password => 'password'}
+        post :get_token, params: { :user => {:email => "firstuser@gmail.com",
+                    :password => 'password'}}
 
         expect(response.status).to be(200)
         expect(response.content_type).to eq "application/json"
         
         returnedJSON = json(response)
         expect(returnedJSON).to have_key("auth_token")
-        expect(decodeResponseJWT(response)['user_id']).to eq(u.id)
+        expect(decodeResponseJWT(response)['user_id']).to eq(@u.id)
       end
+      
     end
 
     context 'Invalid credentials' do
-      it 'should indicate that username is incorrect if it doesnt exist' do
-        post :api_v1_auth, params: {:email => 'random@example.com',
-                    :password => 'password'}
+      it 'should indicate that an account doesnt exist if no one has signed up with that email' do
+        post :get_token, params: { :user => {:email => 'random@example.com',
+                    :password => 'password'}}
 
         expect(response.status).to be(401)
         expect(response.content_type).to eq "application/json"
-        expect(json(response)['errors']).to include('Incorrect Email')
+        expect(json(response)['errors']).to include('There is no account associated with this email')
       end
 
-      it 'should indicate that password is incorrect if username exists' do
-        FactoryBot.create(:user)        
-        post :api_v1_auth, params: {:email => 'user@example.com',
-                    :password => 'nottherightpassword'}
+      it 'should indicate that password is incorrect if inputted password doesnt match password_digest on file ' do
+        post :get_token, params: {:user => {:email => "firstuser@gmail.com",
+                    :password => 'nottherightpassword'}}
 
         expect(response.status).to be(401)
         expect(response.content_type).to eq "application/json"
@@ -48,27 +53,19 @@ RSpec.describe AuthenticationController, type: :controller do
 
   context 'Signing up' do
     it "should complete the sign up process and return a token for a user" do
-      u = FactoryBot.create(:user)
-      post :sign_up, params: {email: u.email, password: u.password}
+      post :sign_up, params: {:user => {email: "validemail@gmail.com", password: "validpassword"}}
       expect(response.status).to eq(200)
       returnedJSON = json(response)
       expect(returnedJSON).to have_key("auth_token")
       decoded = decodeResponseJWT(response)
-      expect(decoded['user_id']).to eq(u.id)
+      expect(User.count).to eq(2)
+      p User.first
     end
 
     it "should return an error the user has already signed up" do
-      u = FactoryBot.create(:user)
-      post :sign_up, params: {email: u.email, password: u.password}
+      post :sign_up, params: {:user => {email: @u.email, password: @u.password}}
       expect(response.status).to eq(401)
-      expect(json(response)['errors']).to eq(["This account has already signed up."])
-    end
-
-    it "should return an error if the email is not found for a user" do
-      u = FactoryBot.create(:user)
-      post :sign_up, params: {email: "missing@oops.com", password: user.password, name: user.name}
-      expect(response.status).to eq(401)
-      expect(json(response)['errors']).to eq(["Incorrect Email"])
+      expect(json(response)['errors']).to eq(["An account has already been created with this email"])
     end
 
   end
@@ -76,7 +73,6 @@ RSpec.describe AuthenticationController, type: :controller do
   context 'Logging out' do
     
     it 'should return an empty token' do
-      FactoryBot.create(:user)
       token = get_valid_token()
       request.headers.merge! build_headers(token)
       delete :sign_out
