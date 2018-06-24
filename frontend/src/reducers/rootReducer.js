dd9b2f4c8d8809e33792f033ede121eee81acd5f
Leland Krych <Leland.Krych@gmail.com>
2018-06-24 09:05:04 -0700

    add errors reducer to rootReducer

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 
 import domainReducer from './domainReducer';
 import sessionReducer from './sessionReducer';
+import errorsReducer from './errorsReducer';
 
 
 const RootReducer = combineReducers({
   domains: domainReducer,
-  session: sessionReducer
+  session: sessionReducer,
+  errors: errorsReducer
 });
 
 export default RootReducer;
