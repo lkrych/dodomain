7dac81335d610dfc093b81afa7648f2b195c6db1
Leland Krych <Leland.Krych@gmail.com>
2018-06-23 17:06:06 -0700

    create a signupform

---
+++ frontend/src/components/SignUpForm.jsx
@@ -0,0 +1,98 @@
+import React, {Component} from 'react';
+
+import TextInput from './reusable/TextInput';
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state =  {
+      errors: '',
+      email: '',
+      password: '',
+      password_confirmation: '',
+    };
+    this.onInput = this.onInput.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  //update state fields
+  onInput (property) {
+    return (e) => {
+      this.setState({ [property]: e.target.value });
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log("hitting onSubmit in signup!"); 
+    if (!this.state.email) {
+      return this.setState({
+        errors: 'Email is required.',
+        message: undefined,
+        disableSubmit: false
+      });
+    }
+    if (this.state.password !== this.state.password_confirmation) {
+      return this.setState({
+        errors: 'Passwords must match.',
+        message: undefined,
+        disableSubmit: false
+      });
+    }
+    if (this.state.password.length < 6 || this.state.password.length > 20 ) {
+      return this.setState({
+        errors: 'Passwords should be at least 6 characters and have fewer than 20 characters.',
+        message: undefined,
+        disableSubmit: false
+      });
+    }
+    this.props.signUp({
+      email: this.state.email,
+      password: this.state.password,
+    });
+  }
+
+  render() {
+    let textErrors = <div className="errors">{this.state.errors}</div>;
+    return (
+      <div>
+        {textErrors}
+        <form>
+        <div className='field'>
+            <label> Email </label>
+            <br />
+            <input autoFocus='autofocus' type='email'
+              id='email' 
+              onChange={this.onInput('email')}
+              value={this.state.email} />
+          </div>
+          <div className='field' >
+            <label> Password </label>
+            <br />
+            <input autoComplete='off' type='password'
+              id='password' 
+              onChange={this.onInput('password')}
+              value={this.state.password}
+                />
+            </div>
+
+          <div className='field' >
+            <label> Password Confirmation </label>
+            <br />
+            <input autoComplete='off' type='password'
+              id='password_confirmation' 
+              onChange={this.onInput('password_confirmation')}
+              value={this.state.password_confirmation}
+                />
+            </div>
+
+          < input
+            type="submit"
+            className="btn btn-primary"
+            onClick={this.onSubmit}/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
