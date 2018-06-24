7f3cf628a4fbac2faa20c29354ff1b8f7b067383
Leland Krych <Leland.Krych@gmail.com>
2018-06-24 15:21:36 -0700

    create consistent styles for homepage

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+
+import dodo from "../resources/images/dodo.jpg";
+
+
+
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    paddingTop: theme.spacing.unit * 3,
+    paddingBottom: 16,
+  }),
+});
 
 class HomePage extends React.Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div className="jumbotron">
-        <h1>Dodomain</h1>
-        <p>Keep track of domains before they go the way of the dodo.</p>
-      </div>
+      <Paper className={classes.root} elevation={4}>
+        <Typography variant="headline" gutterBottom>
+            Dodomain
+        </Typography>
+          
+        <Typography variant="body2" gutterBottom>
+          Keep track of domains before they go the way of the dodo.
+        </Typography>
+        <img src={dodo} alt="painting of dodo" />
+      </Paper>
     );
   }
 }
 
-export default HomePage;
+export default withStyles(styles)(HomePage);
