fb42a04dc527ef9432083fb16347e7306a8618a8
Leland Krych <Leland.Krych@gmail.com>
2018-06-23 17:04:08 -0700

    drop the routes into index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import routes from './routes';
+
+const store = configureStore();
+window.store = store;
+
+ReactDOM.render( 
+  <Provider store={store}>
+    <Router>
+      {routes}
+    </Router>
+  </ Provider>, 
+  document.getElementById('root'));
