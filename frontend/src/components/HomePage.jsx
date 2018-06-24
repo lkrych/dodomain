da080736a6f2330fd32b34626f0b9c9839ca5b22
Leland Krych <Leland.Krych@gmail.com>
2018-06-24 15:05:38 -0700

    remove link to test login

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -7,7 +7,6 @@ class HomePage extends React.Component {
       <div className="jumbotron">
         <h1>Dodomain</h1>
         <p>Keep track of domains before they go the way of the dodo.</p>
-        <Link to="login" className="btn btn-primary btn-lg">log in as a test user</Link>
       </div>
     );
   }
