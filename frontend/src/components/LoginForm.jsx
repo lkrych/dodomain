8e0c048183f71d81da9d0ef1bb7ce8dc31a0a9dd
Leland Krych <Leland.Krych@gmail.com>
2018-06-23 17:05:54 -0700

    create a loginform

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state =  {
+      email: '',
+      password: '',
+    };
+    this.onInput = this.onInput.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  //update state fields
+  onInput (property) {
+    return (e) => {
+      this.setState({ [property]: e.target.value });
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.props.login(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <div className='field'>
+            <label> Email </label>
+            <br />
+            <input autoFocus='autofocus' type='email'
+              id='email' 
+              onChange={this.onInput('email')}
+              value={this.state.email} />
+          </div>
+          <div className='field' >
+            <label> Password </label>
+            <br />
+            <input autoComplete='off' type='password'
+              id='password' 
+              onChange={this.onInput('password')}
+              value={this.state.password}
+                />
+            </div>
+          < input
+            type="submit"
+            className="btn btn-primary"
+            onClick={this.onSubmit}/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
