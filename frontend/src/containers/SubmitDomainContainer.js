1e7d104d1644101238519a5c57ee9841782402a3
Leland Krych <Leland.Krych@gmail.com>
2018-06-24 09:04:13 -0700

    add errors to submitDomaincontainer

--- frontend/src/containers/SubmitDomainContainer.js
+++ frontend/src/containers/SubmitDomainContainer.js
@@ -1,15 +1,17 @@
 import { connect } from 'react-redux';
 
 import SubmitDomain from '../components/SubmitDomain';
-import {submitDomain} from '../actions/actions';
+import {submitDomain, clearErrorsState} from '../actions/actions';
 
 const mapStateToProps = state => ({
-  domains: state.domains
+  domains: state.domains,
+  errors: state.errors.domain_errors
 });
 
 const mapDispatchToProps = dispatch => (
   {
-    submitDomain: (domainInfo) => submitDomain(domainInfo, dispatch)
+    submitDomain: (domainInfo) => submitDomain(domainInfo, dispatch),
+    clearErrors: () => clearErrorsState(dispatch)
   }
 );
 
