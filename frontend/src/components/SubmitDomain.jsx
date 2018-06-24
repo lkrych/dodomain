a8d91fcd4e1c60d3a537e1e01d4fffc7e5fda1ec
Leland Krych <Leland.Krych@gmail.com>
2018-06-24 15:06:51 -0700

    import styles into submitdomain

--- frontend/src/components/SubmitDomain.jsx
+++ frontend/src/components/SubmitDomain.jsx
@@ -1,4 +1,16 @@
 import React, {Component} from 'react';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    paddingTop: theme.spacing.unit * 3,
+    paddingBottom: 16,
+  }),
+});
 
 class SubmitDomain extends Component {
   constructor(props){
@@ -28,37 +40,55 @@ class SubmitDomain extends Component {
   }
 
   render(){
+    const { classes } = this.props;
     let textErrors = <div className="errors">{this.props.errors}</div>;
 
     return (
-      <div>
+      <Paper className={classes.root} elevation={4}>
+      <Typography variant="headline" gutterBottom>
+          Submit a Domain
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          Domain Name ex: "npr.org"
+        </Typography> 
         {textErrors}
         <form action="">
-        <div className='field'>
-            <label> Domain Name ex: "npr.org" </label>
-            <br />
-            <input autoFocus='autofocus' type='name'
+          <div className='field'>
+            <TextField
+              label='Domain Name' 
+              type='name'
               id='name' 
               onChange={this.onInput('name')}
               value={this.state.name} />
           </div>
 
-         <textarea type='text'
+        <div className='field'>
+          <TextField
+            id="description"
             value={this.state.description}
             onChange={this.onInput('description')} 
-            id='description'
             name='description' 
-            cols='80' rows='15' required/>
+            label="Description of domain"
+            placeholder="Description of domain"
+            multiline
+            className={classes.textField}
+            margin="normal"
+          />
+         
+        </div>
 
 
-        < input
+        <Button
           type="submit"
+          variant="contained" color="primary"
           className="btn btn-primary btn-submit"
-          onClick={this.onSubmit}/>
+          onClick={this.onSubmit}>
+          Submit Domain
+        </Button>
         </form>
-      </div>
+      </Paper>
     );
   }
 }
 
-export default SubmitDomain;
+export default withStyles(styles)(SubmitDomain);
