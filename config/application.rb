77295dc84fa54703603ee5b94b595dd2ced68d0c
Leland Krych <Leland.Krych@gmail.com>
2018-06-25 14:51:40 -0700

    try loading lib in a different way in prod

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module Dodomain
     config.load_defaults 5.1
 
     #autoload lib files, most importantly JWT
-    config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
 
 
     # Settings in config/environments/* take precedence over those specified here.
